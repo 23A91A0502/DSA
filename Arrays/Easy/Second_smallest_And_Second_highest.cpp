f2a68820dfebe89e3d55a76f1b6b351c30e5bb46
Madhumitha Akula <23A91A0502@aec.edu.in>
2025-05-03 11:24:42 +0530

    Update Second_smallest_And_Second_highest.cpp

--- Arrays/Easy/Second_smallest_And_Second_highest.cpp
+++ Arrays/Easy/Second_smallest_And_Second_highest.cpp
@@ -1,4 +1,5 @@
-#include<bits/stdc++.h> //better approach
+                                /////better approach///
+#include<bits/stdc++.h> 
 using namespace std;
 int main(){
     vector<int>arr={1,2,5,4};
@@ -30,3 +31,52 @@ int main(){
     cout<<"Second smallest: "<<sec_small<<endl;
     cout<<"Second highest: "<<sec_high;
 }
+
+                                         ////OPTIMAL APPROACH///
+#include<bits/stdc++.h> //OPTIMAL approach
+using namespace std;
+int second_smallest(vector<int>&arr,int n){
+    if(n<2) {return -1;}
+    
+        int s=INT_MAX;
+        int ss=INT_MAX;
+        for(int i=0;i<n;i++){
+            if(arr[i] < s){
+                ss=s;
+                s=arr[i];
+            }
+            else if(arr[i] < ss && arr[i]!=s){
+                ss=arr[i];
+            }
+        }
+    
+    return ss;
+}
+int second_highest(vector<int>&arr,int n){
+    if(n<2){return -1;}
+    
+        int h=INT_MIN;
+        int sh=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(arr[i] > h){
+                sh=h;
+                h=arr[i];
+            }
+            else if(arr[i] > sh && arr[i]!=h){
+                sh=arr[i];
+            }
+        }
+
+    
+    return sh;
+}
+int main(){
+    vector<int>arr={1,2,5,4};
+    int n=arr.size();
+    int second_low=second_smallest(arr,n);
+    cout<<"Second smallest: "<<second_low<<endl;
+    int second_high=second_highest(arr,n);
+    cout<<"Second highest: "<<second_high;
+}
+
+
